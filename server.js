8edfd7322ae4bdfcdf056767219333abaa4878bd
Derek <derekj77@gmail.com>
2019-11-29 13:41:23 -0600

    DB changes

--- server.js
+++ server.js
@@ -14,15 +14,24 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-const db = knex({
-	client: "pg",
-	connection: {
-		host: process.env.DATABASE_HOST || "127.0.0.1",
-		user: process.env.DATABASE_USER || "postgres",
-		password: process.env.DATABASE_PASSWORD || "postgres",
-		database: process.env.DATABASE_NAME || "recognize"
-	}
-});
+const db =
+	NODE_ENV == "production"
+		? knex({
+				client: "pg",
+				connection: {
+					connectionString: process.env.DATABASE_URL,
+					ssl: true
+				}
+		  })
+		: knex({
+				client: "pg",
+				connection: {
+					host: process.env.DATABASE_HOST || "127.0.0.1",
+					user: process.env.DATABASE_USER || "postgres",
+					password: process.env.DATABASE_PASSWORD || "postgres",
+					database: process.env.DATABASE_NAME || "recognize"
+				}
+		  });
 
 const app = express();
 
