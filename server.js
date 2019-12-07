562085d06c997b65bd815e380bcdc3caf8e97112
Derek <derekj77@gmail.com>
2019-12-07 11:43:21 -0600

    Minor updates

--- server.js
+++ server.js
@@ -15,23 +15,23 @@ dotenv.config();
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const db =
-	process.env.NODE_ENV === "production"
-		? knex({
-				client: "pg",
-				connection: {
-					connectionString: process.env.DATABASE_URL,
-					ssl: true
-				}
-		  })
-		: knex({
-				client: "pg",
-				connection: {
-					host: process.env.DATABASE_HOST || "127.0.0.1",
-					user: process.env.DATABASE_USER || "postgres",
-					password: process.env.DATABASE_PASSWORD || "postgres",
-					database: process.env.DATABASE_NAME || "recognize"
-				}
-		  });
+  process.env.NODE_ENV === "production"
+    ? knex({
+        client: "pg",
+        connection: {
+          connectionString: process.env.DATABASE_URL,
+          ssl: true
+        }
+      })
+    : knex({
+        client: "pg",
+        connection: {
+          host: process.env.DATABASE_HOST || "127.0.0.1",
+          user: process.env.DATABASE_USER || "postgres",
+          password: process.env.DATABASE_PASSWORD || "postgres",
+          database: process.env.DATABASE_NAME || "recognize"
+        }
+      });
 
 const app = express();
 
@@ -39,30 +39,30 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-	res.send(db.users);
+  res.send(db.users);
 });
 app.post("/signin", signin.handleSignin(db, bcrypt));
 
 app.post("/register", (req, res) => {
-	register.handleRegister(req, res, db, bcrypt);
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
-	profile.handleProfileGet(req, res, db);
+  profile.handleProfileGet(req, res, db);
 });
 
 app.put("/image", (req, res) => {
-	image.handleImage(req, res, db);
+  image.handleImage(req, res, db);
 });
 
 app.post("/imageurl", (req, res) => {
-	image.handleApiCall(req, res);
+  image.handleApiCall(req, res);
 });
 
 app.get("/test", (req, res) => {
-	res.send("It is working!");
+  res.send("It is working!");
 });
 
 app.listen(port, () => {
-	console.log(`app is running on port ${port}`);
+  console.log(`app is running on port ${port}`);
 });
